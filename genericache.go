7a2db15272e9679364da026d8e349306204802c2
Nathan Johnson <nathan@nathanjohnson.org>
2022-03-06 17:18:35 -0600

    documentation

--- genericache.go
+++ genericache.go
@@ -1,9 +1,13 @@
+// Package genericache is a simple cache with no evictions, no expiration.
 package genericache
 
 import "sync"
 
+// Filler is a function that is meant to fetch values on cache miss.
+// An error returned from this function will be returned from the GeneriCache.Get() method.
 type Filler[K comparable, V any] func(K) (V, error)
 
+// GeneriCache implements a simple cache with no eviction.
 type GeneriCache[K comparable, V any] struct {
 	sync.Mutex
 	entries     map[K]*cacheEntry[V]
@@ -17,6 +21,12 @@ type cacheEntry[V any] struct {
 	err error
 }
 
+// NewGeneriCache - factory method for GeneriCache.  Use this to initialize.
+// f is a filler method meant to fill the cache on cache miss.  retryErrors indicates
+// whether or not an error received from f should indicate to try the filler function
+// on next Get() operation.  If this is false, it will return the same error over and
+// over and not call f.  If true, it will call f on subsequent Get() calls
+// until error is nil.
 func NewGeneriCache[K comparable, V any](f Filler[K, V], retryErrors bool) *GeneriCache[K, V] {
 	return &GeneriCache[K, V]{
 		entries:     make(map[K]*cacheEntry[V]),
@@ -25,6 +35,7 @@ func NewGeneriCache[K comparable, V any](f Filler[K, V], retryErrors bool) *Gene
 	}
 }
 
+// Get - Get a value from the cache.  On cache miss, it will call the Filler function.
 func (gc *GeneriCache[K, V]) Get(k K) (V, error) {
 	gc.Lock()
 	var ce *cacheEntry[V]
